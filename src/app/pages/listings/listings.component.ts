4ae29f48d76bc625c0b2bc7d23e7475972235684
ishutrehan <ishu.bys@gmail.com>
2022-04-13 16:59:45 +0530

    Implement API's, Display properties

--- src/app/pages/listings/listings.component.ts
+++ src/app/pages/listings/listings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilityProvider } from '../../../providers/utilities/utility';
+import { ApiProvider } from '../../../providers/auth/auth';
 
 @Component({
   selector: 'app-listings',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingsComponent implements OnInit {
   searchParams : any;
-  distanceOptions : any
-  constructor() {
-    
+  distanceOptions : any;
+  currentPayload : any = {};
+  offset_value : number;
+  sort_name:any = "recent";
+  propertyList:any = [];
+  constructor(public utility:UtilityProvider, public auth:ApiProvider) {
+    this.offset_value = 0;
   }
 
   ngOnInit(): void {
@@ -17,6 +23,7 @@ export class ListingsComponent implements OnInit {
       sort_by: "recent",
       distance: ""
     };
+
     this.distanceOptions = [
       { name: "2km", value: "2" },
       { name: "5km", value: "5" },
@@ -29,7 +36,45 @@ export class ListingsComponent implements OnInit {
       { name: "40km", value: "40" },
       { name: "45km", value: "45" },
       { name: "50km", value: "50" }
-    ]
+    ];
+    var payload:any = {};
+    payload.sort_by = this.sort_name == 'reset'?"":this.sort_name;
+    payload.offset = this.offset_value;
+    this.getPropertyList(payload);
   }
+  getPropertyList(payload:any ) {
+    this.currentPayload = payload;
+    
+    //console.log("payload is", payload);
+    
+    //this.utility.startLoading();
+    this.auth.getPropertyList(payload)
+      .then((result: any) => {
+       // console.log("propertyList", result);
+        //this.propertyList = [...this.propertyList,...result.properties];
+        this.propertyList = result.properties;
+        // this.allow_skip = result.next;
+        // this.allow_lazy_loading = true;
+        // if(this.propertyList.length == 0){
+        //   this.showList = false;
+        // }else{
+        //   this.showList = true;
 
+        // }
+        //this.utility.stopLoading();
+      }, err => {
+        //console.log("err", err);
+        if(err[0] == 'error') { //user deleted or session expired
+          // this.utility.removeStorage("isLogin");
+          // this.utility.removeStorage('refreshToen');
+          // this.utility.removeStorage('token');
+          // this.utility.showToast('Votre session a expiré ou votre compte est peut-être supprimé');
+          // this.navCtrl.setRoot(LandingPage);
+        }
+       // this.utility.stopLoading();
+      })
+  }
+  SubString(text) {
+    return text = text == null ? '' : text.substring(0, 100) + '..';
+  }
 }
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UtilityProvider } from '../../providers/utilities/utility';
+
 
 /*
   Generated class for the AuthProvider provider.
@@ -8,9 +10,73 @@ import { Injectable } from '@angular/core';
   and Angular DI.
 */
 @Injectable()
+
 export class ApiProvider {
   baseUrl: any;
-  constructor(public http: HttpClient) {
-    this.baseUrl = "http://localhost:3000/api/admin";
-  }  
+  constructor(public http: HttpClient, public utility: UtilityProvider) {
+    this.baseUrl = "https://yedimmobilier.com/api";
+  }
+  // get property listing
+  getPropertyList(val) {
+    
+    return new Promise((resolve, reject) => {
+      var token = this.utility.getStorage('refreshToen');
+      if (token) {
+        this.getHeaders()
+          .then((tokens: any) => {
+            if(!tokens) return reject(['error']);
+            let headers = new HttpHeaders({
+              'Authorization': tokens
+            });
+            return this.http.post(this.baseUrl + '/property/search', val, { headers: headers }).subscribe(data => {
+              resolve(data);
+            }, err => {
+              reject(err);
+              // console.log("ERER:", err);
+            });
+          });
+
+      } else {
+        return this.http.post(this.baseUrl + '/property/search', val).subscribe(data => {
+          resolve(data);
+        }, err => {
+          reject(err);
+          //console.log("ERER:", err);
+        });
+      }
+    });
+  }
+  generateToken() {
+    if(this.utility.getStorage('refreshToken')){
+      var token = this.utility.getStorage('refreshToen');
+      var body = {
+        'refresh_token': token
+      }
+      return new Promise((resolve, reject) => {
+        this.http.post(this.baseUrl + '/user/refresh', body).subscribe(data => {
+          resolve(data);
+        }, err => {
+          reject(err);
+         // console.log("ERER:", err);
+        });
+      });
+    }else{
+      return new Promise((resolve, reject) => {
+       
+          reject();
+      });
+    };
+
+  }
+  getHeaders() {
+    return new Promise((resolve, reject) => {
+      this.generateToken()
+        .then((token: any) => {   
+          resolve(token.access_token);   
+
+        }, err => {
+        })
+    });
+
+  }
 }
