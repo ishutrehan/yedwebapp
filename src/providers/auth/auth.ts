4aecf9622f46e7829c8e216762314dbabe70f29e
ishutrehan <ishu.bys@gmail.com>
2022-04-13 09:49:00 +0530

    add auth provider

---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,16 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the AuthProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApiProvider {
+  baseUrl: any;
+  constructor(public http: HttpClient) {
+    this.baseUrl = "http://localhost:3000/api/admin";
+  }  
+}
