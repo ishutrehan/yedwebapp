00a2b83a6c7443c0bdb930a90c3976962b14854a
ishutrehan <ishu.bys@gmail.com>
2022-05-11 12:15:57 +0530

    add animation library

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { gsap } from "gsap";
+import Draggable from "gsap/Draggable";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-landing',
@@ -7,13 +10,17 @@ import {Router} from '@angular/router';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
+ 
   constructor(private router: Router) { 
 
   }
 
   ngOnInit(): void {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
+    // don't forget to register plugins
+    gsap.registerPlugin(ScrollTrigger, Draggable);
+    
+    this.initScrollTriggers();
   }
   goToLink(link:any){
     if(link == 'listings')
@@ -21,4 +28,18 @@ export class LandingComponent implements OnInit {
     else
       this.router.navigate([link]);
   }
+  initScrollTriggers() {
+    document.querySelectorAll(".content-animation").forEach(box => {
+      const scrollBox = gsap.timeline({
+        scrollTrigger: {
+          trigger: box,
+          start: "20px 80%",
+          end: "bottom bottom",
+          toggleActions: "play none, none, none"
+        }
+      });
+      scrollBox.from(box, { y: 30, opacity: 0, duration: 1, rotation: 45 });
+    });
+  }
+  
 }
