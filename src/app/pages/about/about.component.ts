30486907bf3923e69948503437be89dac2ec4992
ishutrehan <ishu.bys@gmail.com>
2022-05-19 12:21:39 +0530

    changes

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
+    
   }
 
 }
